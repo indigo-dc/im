733f3fa01f1bd59d7579e0180c84564442a5e17e
micafer <micafer1@upv.es>
2019-05-03 09:11:18 +0200

    Fix install ansible

--- contextualization/ansible_install.sh
+++ contextualization/ansible_install.sh
@@ -72,16 +72,8 @@ else
             apt-get -y install software-properties-common wget sudo
             apt-add-repository -y ppa:ansible/ansible
             wget -q -O - https://repo.indigo-datacloud.eu/repository/RPM-GPG-KEY-indigodc | sudo apt-key add -
-            case $(distribution_major_version) in
-                14)
-                    wget https://repo.indigo-datacloud.eu/repository/indigo/1/ubuntu/dists/trusty-updates/main/binary-amd64/indigodc-release_1.0.0-2_amd64.deb
-                    dpkg -i indigodc-release_1.0.0-2_amd64.deb
-                    ;;
-                16)
-                    wget https://repo.indigo-datacloud.eu/repository/indigo/2/ubuntu/dists/xenial/main/binary-amd64/indigodc-release_2.0.0-1_amd64.deb
-                    dpkg -i indigodc-release_2.0.0-1_amd64.deb
-                    ;;
-            esac
+            wget https://repo.indigo-datacloud.eu/repository/indigo/2/ubuntu/dists/xenial/main/binary-amd64/indigodc-release_2.0.0-1_amd64.deb
+            dpkg -i indigodc-release_2.0.0-1_amd64.deb
             apt-get update
             apt-get -y install ansible
             ;;
